81f43096060ffce97295726775c607c0267e69dc
Graham Robinson <graham@studiox.tv>
2018-06-13 09:51:14 -0700

    checkin project

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=onnihelperbot-Web-Deploy.pubxml -p:Password=Ky8qga0cEesFunBgxTivEHoAcYWpTrvHkBdNtHjSESJ4HzEKjLaaucFdsH6q
+
